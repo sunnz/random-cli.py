b19b47baa01996d70c1909d22181b6c7f7162d55
sunnz <sunnz@users.noreply.github.com>
2017-07-25 11:56:54 +1000

    updated unit tests with standardised iterations and length.

--- test_random_cli.py
+++ test_random_cli.py
@@ -7,24 +7,37 @@ import string
 
 class RandomCLITestCase(unittest.TestCase):
     def test_random_alphanumeric_string__len(self):
-        result = random_cli.random_alphanumeric_string(10)
-        self.assertEqual(
-            len(result), 10, 'random_string does not return correct length.')
+        for i in range(10):
+            result = random_cli.random_alphanumeric_string(64)
+            self.assertEqual(
+                len(result), 64,
+                'random_cli.random_alphanumeric_string does not return correct'
+                ' length.')
 
     def test_random_alphanumeric_string__alphanumeric(self):
-        result = random_cli.random_alphanumeric_string(1)
-        alphanumeric = string.ascii_letters + string.digits
-        self.assertIn(result, alphanumeric)
+        for i in range(10):
+            alphanumeric = string.ascii_letters + string.digits
+            result = random_cli.random_alphanumeric_string(64)
+            for char in result:
+                self.assertIn(char, alphanumeric)
 
     def test_random_hex_string__hex(self):
-        result = random_cli.random_hex_string(1)
-        self.assertIn(result, string.hexdigits)
+        for i in range(10):
+            result = random_cli.random_hex_string(64)
+            for char in result:
+                self.assertIn(char, string.hexdigits)
 
-    def test_random_printable_string__printable(self):
-        printable = string.ascii_letters + string.digits + string.punctuation
-        result = random_cli.random_printable_string(10)
-        for char in result:
-            self.assertIn(char, printable)
+    def test_random_printable_string__printable__len(self):
+        for i in range(10):
+            printable = string.ascii_letters + string.digits
+            printable += string.punctuation
+            result = random_cli.random_printable_string(64)
+            self.assertEqual(
+                len(result), 64,
+                'random_cli.random_printable_string does not return correct'
+                ' length.')
+            for char in result:
+                self.assertIn(char, printable)
 
 
 if __name__ == '__main__':
