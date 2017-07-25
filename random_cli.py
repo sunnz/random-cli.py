536772e5f6e568606d0089dbc53f55451246b7f9
sunnz <sunnz@users.noreply.github.com>
2017-07-25 11:21:48 +1000

    added type option for different character sets.

--- random_cli.py
+++ random_cli.py
@@ -6,9 +6,24 @@ import click
 
 
 @click.command()
+@click.option(
+    '--type',
+    default='alphanumeric',
+    help='type of characters to be generated.')
 @click.argument('length', required=False, default=64)
-def random_cli(length):
-    click.echo(random_alphanumeric_string(length))
+def random_cli(type, length):
+    if 'alphanumeric' == type:
+        click.echo(random_alphanumeric_string(length))
+    elif 'hex' == type:
+        click.echo(random_hex_string(length))
+    elif type in ['print', 'printable']:
+        click.echo(random_string(length, string.printable))
+    elif type in ['lower', 'lowercase']:
+        click.echo(random_string(length, string.ascii_lowercase))
+    elif type in ['upper', 'uppercase']:
+        click.echo(random_string(length, string.ascii_uppercase))
+    elif type in ['letter', 'letters', 'alpha']:
+        click.echo(random_string(length, string.ascii_letters))
 
 
 def random_alphanumeric_string(len):
