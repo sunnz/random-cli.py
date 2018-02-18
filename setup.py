6043caa0d67730a34afdd28bfb490b3af193934c
sunnz <sunnz@users.noreply.github.com>
2018-02-18 14:57:09 +1100

    added new python requirement to setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='random_cli',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['random_cli'],
     install_requires=[
         'Click>=5,<7',
     ],
-    python_requires='>=3, <4',
+    python_requires='>=3.6, <4',
     entry_points={
         'console_scripts': [
             'random=random_cli:random_cli',
